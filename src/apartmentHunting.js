1c0107afcfd5f9dbf693adc909e5564f145d19f3
js-scripter <sachin.noder@gmail.com>
2021-03-02 14:11:44 +0530

    added inverted index of all facilities required in an ideal block. This index will group index values of blocks in blocks array according to the facilities like gym, store,school etc.

--- src/apartmentHunting.js
+++ src/apartmentHunting.js
@@ -1,6 +1,16 @@
 class Search{
 	apartmentHunting(blocks, reqs) {
-
-	}
+        //make object (act as inverted index) with properties name equal to facility name in reqs
+        let invertedIndex={}
+        for(let a=0; a<reqs.length; a++){
+            let facility = reqs[a]
+            invertedIndex[facility]=[]
+            blocks.forEach((element,index)=>{
+                if(element[facility]==true){
+                    invertedIndex[facility].push(index)
+                }
+            })
+        }
+    }
 }
 module.exports = Search
