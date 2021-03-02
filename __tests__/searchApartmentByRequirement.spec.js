0ccd1446f16a413dd03284c82a2e3aaed5ce1999
js-scripter <sachin.noder@gmail.com>
2021-03-02 13:58:40 +0530

    test failed with message Search is not a constructor

--- __tests__/searchApartmentByRequirement.spec.js
+++ __tests__/searchApartmentByRequirement.spec.js
@@ -1,1 +1,37 @@
 const Search = require('../src/apartmentHunting')
+const apartmentSearch = new Search()
+
+describe("Search Best Apartment",()=>{
+	test("Example - it should search by blocks and requirements parameters",()=>{
+        const blocks = [
+            {
+                "gym": false,
+                "school": true,
+                "store": false,
+            },
+            {
+                "gym": true,
+                "school": false,
+                "store": false,
+            },
+            {
+                "gym": true,
+                "school": true,
+                "store": false,
+            },
+            {
+                "gym": false,
+                "school": true,
+                "store": false,
+            },
+            {
+                "gym": false,
+                "school": true,
+                "store": true,
+            },
+        ]
+        reqs = ["gym", "school", "store"]
+        const output = 3
+        expect(apartmentSearch.apartmentHunting(blocks,reqs)).toEqual(output)
+    })
+})
