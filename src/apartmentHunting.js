0dbcde60905d2800855c6eb8fbedf1fbed923561
js-scripter <sachin.noder@gmail.com>
2021-03-02 14:34:13 +0530

    seperated out createInvertedIndex function

--- src/apartmentHunting.js
+++ src/apartmentHunting.js
@@ -1,16 +1,25 @@
 class Search{
 	apartmentHunting(blocks, reqs) {
-        //make object (act as inverted index) with properties name equal to facility name in reqs
-        let invertedIndex={}
-        for(let a=0; a<reqs.length; a++){
-            let facility = reqs[a]
-            invertedIndex[facility]=[]
-            blocks.forEach((element,index)=>{
-                if(element[facility]==true){
-                    invertedIndex[facility].push(index)
-                }
-            })
-        }
+       //make object (act as inverted index) with properties name equal to facility name in reqs
+        let invertedIndex = CreateInvertedIndex(reqs, blocks);
     }
 }
 module.exports = Search
+
+function CreateInvertedIndex(reqs, blocks) {
+    let invertedIndex = {};
+    for (let a = 0; a < reqs.length; a++) {
+        let facility = reqs[a];
+        invertedIndex[facility] = [];
+        blocks.forEach((element, index) => {
+            if (element[facility] == true) {
+                invertedIndex[facility].push(index);
+            }
+        });
+    }
+    //sort each array in ascending order
+    for (const property in invertedIndex) {
+        invertedIndex[property].sort();
+    }
+    return invertedIndex;
+}
