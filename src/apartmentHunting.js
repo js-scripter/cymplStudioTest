af2e366f966809138fba210b64bfde2c245420c5
js-scripter <sachin.noder@gmail.com>
2021-03-03 23:52:40 +0530

    removed unused index variables in forEach renamed variable b to blockIndex for more clarity in code

--- src/apartmentHunting.js
+++ src/apartmentHunting.js
@@ -1,10 +1,5 @@
 class Search{
-    constructor(){
-        this.testCounter=0
-    }
-    
 	apartmentHunting(blocks, reqs) {
-        this.testCounter++
        //make object (act as inverted index) with properties name equal to facility name in reqs
         let invertedIndex = CreateInvertedIndex(reqs, blocks);
 
@@ -12,25 +7,25 @@ class Search{
         let travelEfficientBlock= {traveldistance:0,index:-1}
 
         //loop on blocks array
-        for(let b=0, len = blocks.length; b<len; b++){
+        for(let blockIndex=0, len = blocks.length; blockIndex<len; blockIndex++){
             // check one block at a time to find distance of each facility
-            let block = blocks[b]
+            let block = blocks[blockIndex]
             // to keep track of distance between current block in iteration and closest facility block
             let currentBlockToCurrentFacilityDistance=0
             // to keep track of most long distance the current block in iteration has 
             let farthestDistanceToTravelOfCurrentBlock=0
 
             // refer facilities to find out if current block has it or need to travel to other block to avail it
-            reqs.forEach((facility,index)=>{
+            reqs.forEach((facility)=>{
                 // search nearest block with given facility in inverted index 
                 let nearestBlockIndex = invertedIndex[facility].reduce(function(prev, curr) {
-                    return (Math.abs(curr - b) < Math.abs(prev - b) ? curr : prev);
+                    return (Math.abs(curr - blockIndex) < Math.abs(prev - blockIndex) ? curr : prev);
                 });
                 // now find the distance to nearest block from the current block
-                if(nearestBlockIndex < b){
-                    currentBlockToCurrentFacilityDistance = b - nearestBlockIndex 
+                if(nearestBlockIndex < blockIndex){
+                    currentBlockToCurrentFacilityDistance = blockIndex - nearestBlockIndex 
                 }else{
-                    currentBlockToCurrentFacilityDistance = nearestBlockIndex - b
+                    currentBlockToCurrentFacilityDistance = nearestBlockIndex - blockIndex
                 }
 
                 //keep farthestDistanceToTravelOfCurrentBlock updated with new long distances found
@@ -40,11 +35,11 @@ class Search{
             })
             // if it is first iteration on blocks array then assign farthestDistanceToTravelOfCurrentBlock 
             // a value of first block to travelEfficientBlock.travelDistance later on b will never be zero 
-            // if maximum/farthest distance of current iterating block is less than most travel
-            // efficient block then the current iterating block becomes most travel efficient block
-            if(b==0 || farthestDistanceToTravelOfCurrentBlock < travelEfficientBlock.traveldistance){
+            // if maximum/farthest distance of current iterating block is less than most travelEfficient block 
+            // then the current iterating block becomes most travel efficient block
+            if(blockIndex==0 || farthestDistanceToTravelOfCurrentBlock < travelEfficientBlock.traveldistance){
                 travelEfficientBlock.traveldistance = farthestDistanceToTravelOfCurrentBlock
-                travelEfficientBlock.index = b
+                travelEfficientBlock.index = blockIndex
             }
         }
         // finally return the most travel efficient block index
@@ -55,7 +50,7 @@ module.exports = Search
 
 function CreateInvertedIndex(reqs, blocks) {
     let invertedIndex = {};
-    reqs.forEach((facility,index)=>{
+    reqs.forEach((facility)=>{
         invertedIndex[facility]=[]
         blocks.forEach((block,index)=>{
             if (block[facility] == true) {
@@ -64,6 +59,5 @@ function CreateInvertedIndex(reqs, blocks) {
             }
         })
     })
-    // console.log(JSON.stringify(invertedIndex))
     return invertedIndex;
 }
